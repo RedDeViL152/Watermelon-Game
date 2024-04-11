63296fea95330e222643d3f93a3b80b58495ff2a
Aung Phyo Ko <105736539+RedDeViL152@users.noreply.github.com>
2024-04-11 22:50:01 +0630

    Out of index range
    Solved the problem of out of index range.

---
+++ Assets/Scripts/Objects/FinalObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalObject : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool inTheAir = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (transform.position.y < 3f)
+        {
+            inTheAir = false;
+            rb.gravityScale = 1;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Drop();
+    }
+
+    public void Drop()
+    {
+        if (inTheAir == true)
+        {
+            GetComponent<Transform>().position = Player.startPosition;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.gravityScale = 1;
+            inTheAir = false;
+            ObjectSpawner.spawnedYet = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == gameObject.tag)
+        {
+            ObjectSpawner.spawnPosition = transform.position;
+            ObjectSpawner.newObjectSpawned = false;
+
+            Destroy(gameObject);
+        }
+    }
+}
