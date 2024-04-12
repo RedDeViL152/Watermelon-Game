3900d48398738bba3609828cad7055252f4c4d69
Neville <103945243+nevillekyaw001@users.noreply.github.com>
2024-04-12 23:11:34 +0700

    Updated SceneLoader singleton and scene essential prefabs

--- Assets/Scripts/Scene Management/SceneLoader.cs
+++ Assets/Scripts/Scene Management/SceneLoader.cs
@@ -1,25 +1,39 @@
-using EasyUnityInternals;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : SingletonMB<SceneLoader>
 {
     public static SceneLoader GetInstance => GetOrCreateInstance();
+    public static LoadSceneMode LoadSceneMode = LoadSceneMode.Additive;
+    public static bool Unpause;
+
+    public static Action onSceneLoad;
     public const string MAIN_MENU_SCENE = "Main";
     public const string GAME_PLAY_SCENE = "Game";
 
 
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+    #region STATIC METHODS
+    public static void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+    public static void LoadScene(string sceneName, LoadSceneMode mode) => SceneManager.LoadScene(sceneName, mode);
+    public static void LoadSceneAsync(string sceneName, LoadSceneMode mode) => SceneManager.LoadSceneAsync(sceneName, mode);
+    public static void UnloadSceneAsync(string sceneName) => SceneManager.UnloadSceneAsync(sceneName);
+    public static void UnloadSceneAsync(Scene scene) => SceneManager.UnloadSceneAsync(scene);
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    protected override void Awake()
     {
-        
+        if (transform.IsChildOf("SceneEssential"))
+        {
+            Log.QuickDebug($"SceneFader.Awake(): Scene: {SceneManager.GetActiveScene().name}");
+            Instance = this;
+            if (!gameObject.activeSelf)
+                gameObject.SetActive(true);
+        }
+        else
+        {
+            Log.Error($"SceneFader initialized from outside SceneEssential. FullName: {gameObject.GetFullName()}");
+            Destroy(this);
+        }
     }
 
     public void SceneInitialize()
